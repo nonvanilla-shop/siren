a439d670f00b7e97e8676c3369f2c7bfc660f053
Marco Papula <m.papula@gmx.at>
2021-10-19 18:48:58 +0200

    update test

--- test/siren_test.dart
+++ test/siren_test.dart
@@ -5,13 +5,19 @@ import 'package:siren/siren/siren_ios.dart';
 
 void main() {
   test('gets new version for ios', () async {
-    final version = await SirenIOS.getVersion(bundleId: 'com.google.Maps');
+    final version = await SirenIOS.getVersion(
+      bundleId: 'com.google.Maps',
+      throwExceptions: true,
+      country: 'US',
+    );
     expect(version, isNotNull);
   });
 
   test('gets new version for android', () async {
-    final version =
-        await SirenAndroid.getVersion(from: 'net.wooga.switchcraft.googleplay');
+    final version = await SirenAndroid.getVersion(
+      from: 'net.wooga.switchcraft.googleplay',
+      throwExceptions: true,
+    );
     expect(version, isNotNull);
   });
 }
