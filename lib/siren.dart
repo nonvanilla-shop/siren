907db71f60e03153716dcf63b759aa344c97edb2
Marco Papula <m.papula@gmx.at>
2021-10-20 13:05:57 +0200

    add open store callback to mapPolicy

--- lib/siren.dart
+++ lib/siren.dart
@@ -6,8 +6,9 @@ import 'package:package_info/package_info.dart';
 import 'package:siren/siren/siren_android.dart';
 import 'package:siren/siren/siren_ios.dart';
 import 'package:siren/siren/version.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-typedef VersionCallback<T> = T Function(Version oldV, Version newV);
+typedef VersionCallback<T> = T Function(Future<void> Function() openStore);
 
 /// This class provides access to the current and optionally the new version of
 /// your app on both Android and iOS.
@@ -29,17 +30,29 @@ class Siren {
     return Platform.isIOS
         ? await SirenIOS.getVersion(
             bundleId: package,
-            throwExceptions: throwExceptions,
             country: country,
           )
         : Platform.isAndroid
-            ? await SirenAndroid.getVersion(
-                from: package,
-                throwExceptions: throwExceptions,
-              )
+            ? await SirenAndroid.getVersion(from: package)
             : throw PlatformException(
                 code: 'Platform is neither iOS nor Android!',
               );
+  }
+
+  /// Returns the new version if it is able to get one
+  Future<void> openAppStore({
+    String country = 'US',
+  }) async {
+    final package = await getPackage();
+    final link = Platform.isIOS
+        ? await SirenIOS.getStoreLink(bundleId: package, country: country)
+        : Platform.isAndroid
+            ? SirenAndroid.getStoreLink(bundleId: package, country: country)
+            : throw PlatformException(
+                code: 'Platform is neither iOS nor Android!',
+              );
+
+    await launch(link.toString());
   }
 
   /// Returns true if a newer version is available.
@@ -53,15 +66,18 @@ class Siren {
     required VersionCallback<T> onYChanged,
     required VersionCallback<T> onZChanged,
     required VersionCallback<T> onBugfixChanged,
+    String country = 'US',
   }) async {
     final oldV = await getCurrentVersion();
     final newV = await getNewVersion();
 
     if (newV == null) return Future.value(null);
 
-    if (oldV.x < newV.x) return onXChanged(oldV, newV);
-    if (oldV.y < newV.y) return onYChanged(oldV, newV);
-    if (oldV.z < newV.z) return onZChanged(oldV, newV);
-    if (oldV.bugfix < newV.bugfix) return onBugfixChanged(oldV, newV);
+    Future<void> openStore() async => openAppStore(country: country);
+
+    if (oldV.x < newV.x) return onXChanged(openStore);
+    if (oldV.y < newV.y) return onYChanged(openStore);
+    if (oldV.z < newV.z) return onZChanged(openStore);
+    if (oldV.bugfix < newV.bugfix) return onBugfixChanged(openStore);
   }
 }
