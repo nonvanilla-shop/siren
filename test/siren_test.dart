b394c7e99b541ef8ea3df8bcb85f9ce9e1ac7429
Marco Papula <m.papula@gmx.at>
2021-10-20 13:06:24 +0200

    fix tests

--- test/siren_test.dart
+++ test/siren_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:siren/siren.dart';
 import 'package:siren/siren/siren_android.dart';
 
 import 'package:siren/siren/siren_ios.dart';
@@ -7,7 +8,6 @@ void main() {
   test('gets new version for ios', () async {
     final version = await SirenIOS.getVersion(
       bundleId: 'com.google.Maps',
-      throwExceptions: true,
       country: 'US',
     );
     expect(version, isNotNull);
@@ -16,7 +16,6 @@ void main() {
   test('gets new version for android', () async {
     final version = await SirenAndroid.getVersion(
       from: 'net.wooga.switchcraft.googleplay',
-      throwExceptions: true,
     );
     expect(version, isNotNull);
   });
